e55d6fa5831ef431db1449d13eb3cb9f5aaf5b03
David Tauraso <david318@me.com>
2019-10-16 15:22:00 -0700

    Finished this assn

--- assignments/this.js
+++ assignments/this.js
@@ -13,7 +13,8 @@
         apply - immediately invokes the function and passes in an array of arguments
         bind - returns a new function that you invoke later.  You pass in arguments  1 by 1
 * 4. 
-
+        New binding
+        using the "new" keyword to make a new object that "this" points to
 *
 * write out a code example of each explanation above
 */
@@ -42,6 +43,12 @@ myObject.object()
 // Principle 3
 
 // code example for New Binding
+function myObjectFunction(something) {
+    // this.something is different cause of "this"
+    this.something = something
+}
+let myNewObject = new myObjectFunction("string")
+console.log(myNewObject.something)
 
 // Principle 4
 
